fe7eb790508200d49d92ac32ed0e919e1ebcf563
davvidbaker <davvidbaker@gmail.com>
2019-08-22 09:44:50 -0600

    small changes

--- on-node-create.js
+++ on-node-create.js
@@ -1,16 +1,4 @@
-var _extends =
-  Object.assign ||
-  function(target) {
-    for (var i = 1; i < arguments.length; i++) {
-      var source = arguments[i];
-      for (var key in source) {
-        if (Object.prototype.hasOwnProperty.call(source, key)) {
-          target[key] = source[key];
-        }
-      }
-    }
-    return target;
-  };
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 const grayMatter = require(`gray-matter`);
 const crypto = require(`crypto`);
@@ -20,21 +8,17 @@ module.exports = async function onCreateNode({
   node,
   getNode,
   loadNodeContent,
-  boundActionCreators,
+  boundActionCreators
 }) {
   const { createNode, createParentChildLink } = boundActionCreators;
 
   // We only care about whoa content, but we'll support markdown too I guess.
-  if (
-    node.internal.mediaType !== `text/markdown` &&
-    node.internal.mediaType !== `text/x-markdown` &&
-    node.extension !== 'whoa'
-  ) {
+  if (node.internal.mediaType !== `text/markdown` && node.internal.mediaType !== `text/x-markdown` && node.extension !== 'whoa') {
     return;
   }
 
-  const nodeContent = await loadNodeContent(node);
-  let data = grayMatter(nodeContent);
+  const content = await loadNodeContent(node);
+  let data = grayMatter(content);
 
   // Convert date objects to string. Otherwise there's type mismatches
   // during inference as some dates are strings and others date objects.
@@ -48,36 +32,27 @@ module.exports = async function onCreateNode({
     });
   }
 
-  const contentDigest = crypto
-    .createHash(`md5`)
-    .update(JSON.stringify(data))
-    .digest(`hex`);
+  const contentDigest = crypto.createHash(`md5`).update(JSON.stringify(data)).digest(`hex`);
   const markdownNode = {
     id: `${node.id} >>> Whoa`,
     children: [],
     parent: node.id,
     internal: {
-      content: data.content,
+      content,
       contentDigest,
-      type: `Whoa`,
-    },
+      type: `Whoa`
+    }
   };
 
-  markdownNode.frontmatter = _extends(
-    {
-      title: ``,
-    },
-    data.data,
-    {
-      _PARENT: node.id,
-      // TODO Depreciate this at v2 as much larger chance of conflicting with a
-      // user supplied field.
-      parent: node.id,
+  markdownNode.frontmatter = _extends({
+    title: `` }, data.data, {
+    _PARENT: node.id,
+    // TODO Depreciate this at v2 as much larger chance of conflicting with a
+    // user supplied field.
+    parent: node.id
 
-      // Add path to the markdown file path
-    }
-  );
-  if (node.internal.type === `File`) {
+    // Add path to the markdown file path
+  });if (node.internal.type === `File`) {
     markdownNode.fileAbsolutePath = node.absolutePath;
   }
 
